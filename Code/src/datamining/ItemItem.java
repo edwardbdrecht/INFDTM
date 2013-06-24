bcfbdafb23149716c78da02a9419ae337dcd7435
Edward Roozeboom <info@edwardroozeboom.com>
2013-06-24 23:35:19 +0200

    OneSlope FTW!!!!

--- Code/src/datamining/ItemItem.java
+++ Code/src/datamining/ItemItem.java
@@ -9,14 +9,18 @@ public class ItemItem
     private int[] userIds;
     private int[] itemIds;
     private float[][] ratings;
+    private float[][] oneSlope;
     
-    // Build table with ratings
+    /*
+     * Build table with ratings
+     */
     public void buildRatingTable(TreeMap<Integer, UserPreferences> userPrefs)
     {
         // Instantiate everything
         userIds = new int[0];
         itemIds = new int[0];
         ratings = new float[0][0];
+        oneSlope = new float[0][0];
         
         // Fill the userIds and itemIds
         for(int i = 0; i < userPrefs.size(); i++)
@@ -31,7 +35,7 @@ public class ItemItem
             }
         }
         
-        // Give rating table it's new size
+        // Give rating table its new size
         ratings = new float[this.userIds.length][this.itemIds.length];
         
         // Fill the rating table
@@ -43,8 +47,34 @@ public class ItemItem
                 ratings[i][pos] = userPrefs.get(i+1).getRatings()[c];
             }
         }
+        
+        // Create the One Slope
+        oneSlope = new float[this.itemIds.length][this.itemIds.length];
+        for(int i = 0; i < this.itemIds.length; i++)
+        {
+            for(int c = 0; c < this.itemIds.length; c++)
+            {
+                float totalDif = 0.0f;
+                int totalRatings = 0;
+                for(int a = 0; a < userIds.length; a++)
+                {
+                    if(ratings[a][i] != 0.0 && ratings[a][c] != 0.0)
+                    {
+                        totalDif += ratings[a][c] - ratings[a][i];
+                        totalRatings++;
+                    }
+                }
+                if(totalRatings != 0)
+                    totalDif = totalDif / totalRatings;
+                oneSlope[i][c] = totalDif;
+            }
+        }
+        
     }
     
+    /*
+     * Adds an item into an array at its proper position
+     */
     private int[] addItemId(int id, int[] sourceArr)
     {        
         int key = Arrays.binarySearch(sourceArr, id); //search in the itemId's array for a corresponding id.
@@ -68,6 +98,9 @@ public class ItemItem
         return sourceArr;
     }
     
+    /*
+     * Prints all data. For debugging. FTW
+     */
     public void printALl()
     {        
         // Table print
@@ -86,6 +119,26 @@ public class ItemItem
             for(int c=0; c < itemIds.length; c++)
             {
                 System.out.print(ratings[i][c]+" ");
+            }
+            System.out.println("");
+        }
+        
+        // OneSlope print
+        System.out.print("   ");
+        for(int a=0; a < itemIds.length; a++)
+        {
+            if(itemIds[a] < 10)
+                System.out.print(itemIds[a]+"   ");
+            else
+                System.out.print(itemIds[a]+"  ");
+        }
+        System.out.println("");
+        for(int i = 0; i < itemIds.length; i++)
+        {
+            System.out.print(itemIds[i] + "  ");
+            for(int c=0; c < itemIds.length; c++)
+            {
+                System.out.print(oneSlope[i][c]+" ");
             }
             System.out.println("");
         }
